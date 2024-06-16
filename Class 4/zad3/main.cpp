cf139ab4690823ad9052dc6d7d65fb537cf9ff36
Cacko12e <117995324+Cacko12e@users.noreply.github.com>
2024-06-16 16:16:27 +0200

    DELUMNO TOCNA
    3 tocni mi printa 3 gresni

--- Class 4/zad3/main.cpp
+++ Class 4/zad3/main.cpp
@@ -1,8 +1,138 @@
 #include<iostream>
-
+#include <cstring>
 using namespace std;
 
-// вашиот код
+class Avtomobil{
+private:
+    char n[20];
+    char brende[20];
+    char model[20];
+    void copy(const Avtomobil&a){
+        strcpy(this->n,a.n);
+        strcpy(this->brende,a.brende);
+        strcpy(this->model,a.model);
+    }
+public:
+    Avtomobil(){
+        strcpy(n,"");
+        strcpy(brende,"");
+        strcpy(model,"");
+    }
+    Avtomobil(char *n,char *brend,char *model){
+        strcpy(this->n,n);
+        strcpy(this->brende,brend);
+        strcpy(this->model,model);
+    }
+    Avtomobil(const Avtomobil&a){
+        copy(a);
+    }
+    Avtomobil&operator=(const Avtomobil&a){
+        if(this!=&a){
+            copy(a);
+        }
+        return *this;
+    }
+    ~Avtomobil(){}
+    void pecati(){
+        cout << n <<" "<< brende << " " << model << endl;
+    }
+
+
+
+};
+class ParkingPlac{
+private:
+    char adresa[20];
+    char *id;
+    int cena;
+    int zarabotka;
+    Avtomobil *avtomobil;
+    int parkirani;
+
+    void copy(const ParkingPlac&p){
+        strcpy(this->adresa,p.adresa);
+        this->id=new char[strlen(p.id)+1];
+        strcpy(this->id,p.id);
+        this->cena=p.cena;
+        this->zarabotka=p.zarabotka;
+        this->avtomobil=new Avtomobil[p.parkirani];
+        for(int i=0;i<p.parkirani;i++){
+            this->avtomobil[i]=p.avtomobil[i];
+        }
+        this->parkirani=p.parkirani;
+    }
+public:
+    ParkingPlac(){
+        strcpy(this->adresa,"");
+        this->id=new char[1];
+        strcpy(this->id,"");
+        this->cena=0;
+        this->zarabotka=0;
+        this->avtomobil=new Avtomobil[1];
+        this->parkirani=0;
+    }
+    ParkingPlac(char *adresa,char *id,int cena){
+        strcpy(this->adresa,adresa);
+        this->id=new char[strlen(id)+1];
+        strcpy(this->id,id);
+        this->cena=cena;
+        this->zarabotka=0;
+        this->avtomobil=new Avtomobil[1];
+        this->parkirani=0;
+    }
+    ParkingPlac(const ParkingPlac&p){
+        copy(p);
+    }
+    ParkingPlac&operator=(const ParkingPlac&p){
+        if(this!=&p){
+            delete[]id;
+            delete[]avtomobil;
+            copy(p);
+        }
+        return *this;
+    }
+    ~ParkingPlac(){
+        delete[]id;
+        delete[]avtomobil;
+    }
+
+    char *getId(){
+        return id;
+    }
+    void platiCasovi(int casovi){
+        zarabotka+=casovi*cena;
+    }
+
+    bool daliIstaAdresa(ParkingPlac p){
+        return strcpy(this->adresa,p.adresa)==0;
+    }
+
+    void parkirajVozilo(const Avtomobil &novoVozilo){
+        Avtomobil*tmp=new Avtomobil[parkirani+1];
+        for(int i=0;i<parkirani;i++){
+            tmp[i]=avtomobil[i];
+        }
+        tmp[parkirani++]=novoVozilo;
+        delete[]avtomobil;
+        avtomobil=tmp;
+    }
+    void pecati() const {
+        if (zarabotka != 0) {
+            cout << id << " " << adresa << " - " << zarabotka << " denari" << endl;
+        } else {
+            cout << id << " " << adresa << endl;
+        }
+    }
+
+    void pecatiParkiraniVozila(){
+        cout<<"Vo parkingot se parkirani slednite vozila:"<<endl;
+        for(int i=0;i<parkirani;i++){
+            cout<<i+1<<".";
+            avtomobil[i].pecati();
+        }
+    }
+
+};
 
 int main(){
 
